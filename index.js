87dce89e052686f34a8d93f27067a03a6913740b
Zell Liew <zellwk@gmail.com>
2016-01-12 02:43:19 +0800

    Add user functionality
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const socketioJwt = require('socketio-jwt');
 const md = require('markdown').markdown;
 const db = mongoose.connection;
 
-const DB_URL = process.env.DB_URL;
 const Message = require('./collections/message');
 const Room = require('./collections/room');
 const User = require('./collections/user');
 
+const DB_URL = process.env.DB_URL;
 mongoose.connect(DB_URL);
 
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -30,7 +30,87 @@ io.use(socketioJwt.authorize({
 }));
 
 io.on('connection', function(socket) {
-  console.log('A user connected.', socket.decoded_token.name);
+  console.log('user connected');
+
+  socket.on('get user', function(username) {
+    User.findOne({username: username}, (err, user) => {
+      if (err) return socket.emit('get user', err, null);
+      if (!user) {
+        return socket.emit('get user', {message: 'No such user'}, null);
+      }
+
+      socket.emit('get user', err, {
+        username: user.username,
+        rooms: user.rooms
+      });
+    });
+  });
+
+  socket.on('create user', req => {
+    var user = new User({
+      username: req.username,
+      rooms: req.rooms || []
+    });
+
+    user.save()
+    .then(data => {
+      socket.emit('create user', null, 'User created');
+    })
+    .catch(err => {
+      socket.emit('create user', err, null);
+    });
+  });
+
+  socket.on('join room', function(data) {
+    if (!data.user) {
+      return socket.emit('join room', 'Join room requires user', null);
+    }
+    if (!data.room) {
+      return socket.emit('join room', 'no specified room to join', null);
+    }
+
+    User.update(
+      {username: data.user},
+      {$addToSet: {rooms: data.room}},
+      (err, data) => {
+        if (err) return socket.emit('join room', err, null);
+      }
+    );
+
+    socket.join(data.room, function(err) {
+      socket.emit('join room', err, `Joined ${data.room}`);
+    });
+  });
+
+  socket.on('leave room', function(data) {
+    console.log('leaving room');
+    if (!data.user) {
+      return socket.emit('leave room', 'leave room requires user', null);
+    }
+    if (!data.room) {
+      return socket.emit('leave room', 'no specified room to leave', null);
+    }
+
+    User.update(
+      {username: data.user},
+      {$pull: {rooms: data.room}},
+      (err, data) => {
+        if (err) return socket.emit('leave room', err, null);
+      }
+    );
+
+    socket.leave(data.room, function(err) {
+      socket.emit('leave room', err, `Left ${data.room}`);
+    });
+  });
+
+  // Note: Broadcasts to everyone in the room except for self.
+  socket.on('message room', function(data) {
+    socket.to(data.room).emit('message room', {
+      message: data.message
+    });
+  });
+
   // send chat log on new user connection
   Message.model('Message').find(function(err, messages) {
     if (err) return console.error(err);
@@ -38,21 +118,20 @@ io.on('connection', function(socket) {
   });
 
   socket.on('chat message', function(msg) {
-    // save message to database
     msg.message = md.toHTML(msg.message);
     const message = new Message(msg);
     message.save(function(err) {
       if (err) return console.error(err);
     });
-    console.log(`Message: ${msg.message}`);
-    // send message to all clients
+    // console.log(`Message: ${msg.message}`);
     io.emit('chat message', msg);
   });
+
   socket.on('typing', function(input) {
-      io.emit('typing', input);
-      console.log(input);
-    });
-  // socket.on('isTyping', input {})
+    io.emit('typing', input);
+    // console.log(input);
+  });
+
   socket.on('disconnect', function() {
     console.log('User disconnected.');
   });
